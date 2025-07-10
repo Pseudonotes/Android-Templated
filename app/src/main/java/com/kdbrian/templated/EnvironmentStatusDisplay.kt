509928d7461241f396509df3063c586ce2e4c020
kidiga <juniorprogrammer09@gmail.com>
2025-07-11 01:56:15 +0300

    update Ui code

--- app/src/main/java/com/kdbrian/templated/EnvironmentStatusDisplay.kt
+++ app/src/main/java/com/kdbrian/templated/EnvironmentStatusDisplay.kt
@@ -2,12 +2,15 @@ package com.kdbrian.templated
 
 import android.content.Context
 import android.net.ConnectivityManager
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -16,6 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -26,7 +31,7 @@ fun EnvironmentStatusDisplay(
     env: String = BuildConfig.ENV,
     modifier: Modifier = Modifier
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = modifier.fillMaxSize()) {
 
         Card(
             modifier = Modifier
@@ -50,14 +55,32 @@ fun EnvironmentStatusDisplay(
             elevation = CardDefaults.cardElevation(8.dp),
             shape = RoundedCornerShape(12.dp)
         ) {
-            Text(
-                text = "Environment: $env",
-                modifier = Modifier.padding(16.dp)
-            )
+            Column(
+                modifier = Modifier.padding(
+                    horizontal = 12.dp,
+                    vertical = 6.dp
+                ),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Text(
+                    text = "Environment: $env",
+                    style = MaterialTheme.typography.displayMedium.copy(
+                        textAlign = TextAlign.Center
+                    )
+//                    modifier = Modifier.padding(16.dp)
+                )
+
+
+                Text(
+                    text = BuildConfig.VERSION_NAME,
+                    fontWeight = FontWeight.Light,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
         }
     }
 }
-
 
 
 @Preview(name = "Connected - DEV")
@@ -79,9 +102,6 @@ fun PreviewConnectedProd() {
 }
 
 
-
-
-
 @Composable
 fun rememberIsConnected(): Boolean {
     val context = LocalContext.current
@@ -89,7 +109,8 @@ fun rememberIsConnected(): Boolean {
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     }
 
-    val networkState = rememberUpdatedState(newValue = connectivityManager.activeNetworkInfo?.isConnected == true)
+    val networkState =
+        rememberUpdatedState(newValue = connectivityManager.activeNetworkInfo?.isConnected == true)
 
     // For demo purposes; for production use NetworkCallback instead.
     return networkState.value

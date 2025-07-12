513d3e760ce9f40017289eb08d65b31f3bb7b882
kidiga <juniorprogrammer09@gmail.com>
2025-07-12 03:23:57 +0300

    TemplatedApp declaration + timber addition for logging

--- app/src/main/java/com/kdbrian/templated/TemplatedApp.kt
+++ app/src/main/java/com/kdbrian/templated/TemplatedApp.kt
@@ -1,11 +1,13 @@
 package com.kdbrian.templated
 
 import android.app.Application
+import timber.log.Timber
 
-class TemplatedApp  : Application(){
+class TemplatedApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
     }
 }

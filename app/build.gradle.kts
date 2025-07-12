fe07b1e9e56062907104b880884289ae209cf721
kidiga <juniorprogrammer09@gmail.com>
2025-07-12 05:21:08 +0300

    refactor/ - up minSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kdbrian.templated"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
